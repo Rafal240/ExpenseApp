156e05fca690069b9480432b4160bfdfbd478dff
Rafal <kuna240@gmail.com>
2023-04-11 13:07:04 +0100

    sectionOne

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -21,6 +22,6 @@ function App() {
       <Expenses item={expenses} />
     </div>
   );
-}
+};
 
 export default App;
